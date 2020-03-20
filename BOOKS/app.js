1735fdc341289c13d63ff1dc014ac7389cd90bd0
Georgi Kyosev <g.kyosev86@gmail.com>
2020-03-20 11:54:34 +0200

    Implement eventHandler

--- BOOKS/app.js
+++ BOOKS/app.js
@@ -1,1 +1,20 @@
-console.log('My requests...')
+import { elements, extractFormData } from "./helpers/helper.js";
+
+
+function createBook(event) {
+    event.preventDefault();
+    const book = extractFormData();
+    console.log(book);
+
+}
+
+function eventHandler() {
+
+    elements.submitBtnRef.addEventListener('click', createBook);
+
+}
+
+eventHandler();
+
+
+
